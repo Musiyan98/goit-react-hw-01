686fb270c6eab48ce11ada3e2b1fbab4982eda98
Musiyan98 <musiyan98@gmail.com>
2024-02-23 21:39:16 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import css from './App.module.css';
 import Profile from './profile/Profile';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './transactionHistory/TransactionHistory';
@@ -8,11 +9,14 @@ import transactionList from './data/transactions.json';
 
 function App() {
   return (
-    <>
+    <div className={css.appContainer}>
+      <h1 className={css.appTittle}>My Profile</h1>
       <Profile {...userData} />
+      <h1 className={css.appTittle}>My friends</h1>
       <FriendList friends={friends} />
+      <h1 className={css.appTittle}>My Transaction history</h1>
       <TransactionHistory transactionList={transactionList} />
-    </>
+    </div>
   );
 }
 
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,13 +3,10 @@ import css from './Profile.module.css';
 const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div className={css.profile}>
-      <div>
-        <img className={css.avatar} src={avatar} alt={username} />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>@{tag}</p>
-        <p className={css.location}>{location}</p>
-      </div>
-
+      <img className={css.avatar} src={avatar} alt={username} />
+      <p className={css.name}>{username}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
       <ul className={css.stats}>
         <li className={css.statsItem}>
           <span className={css.label}>Followers</span>
